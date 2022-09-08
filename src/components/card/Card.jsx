1076e519fa2e0e2c0853c7932a4981aecba24a01
mfaiz6 <Mohammafaiz_2019bcse002@nitsri.net>
2022-09-08 16:48:21 +0530

    Add data_sample.js file and use .map function

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,122 +1,40 @@
 import './card.css'
+import sample_data from "../../data_sample.js"
 
 const Card = () => {
+
+
     return (
+
+
         <div class="row row-cols-1 row-cols-md-3 g-4">
 
-            <div class="col">
-                <div class="card h-100">
-                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp" class="card-img-top" alt="Skyscrapers" />
-                    <div class="card-body">
-                        <h5 class="card-title">John Doe</h5>
-                        <p class="card-text">
-                            Phone: <a href='tel:+12345'>12345</a> <br />
-                            Email: <a href='mailto:john@test.co'>john@test.co</a>
-                        </p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted">Last updated 3 mins ago</small>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col">
-                <div class="card h-100">
-                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp" class="card-img-top" alt="Skyscrapers" />
-                    <div class="card-body">
-                        <h5 class="card-title">John Doe</h5>
-                        <p class="card-text">
-                            Phone: <a href='tel:+12345'>12345</a> <br />
-                            Email: <a href='mailto:john@test.co'>john@test.co</a>
-                        </p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted">Last updated 3 mins ago</small>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col">
-                <div class="card h-100">
-                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp" class="card-img-top" alt="Skyscrapers" />
-                    <div class="card-body">
-                        <h5 class="card-title">John Doe</h5>
-                        <p class="card-text">
-                            Phone: <a href='tel:+12345'>12345</a> <br />
-                            Email: <a href='mailto:john@test.co'>john@test.co</a>
-                        </p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted">Last updated 3 mins ago</small>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="col">
-                <div class="card h-100">
-                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp" class="card-img-top" alt="Skyscrapers" />
-                    <div class="card-body">
-                        <h5 class="card-title">John Doe</h5>
-                        <p class="card-text">
-                            Phone: <a href='tel:+12345'>12345</a> <br />
-                            Email: <a href='mailto:john@test.co'>john@test.co</a>
-                        </p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted">Last updated 3 mins ago</small>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col">
-                <div class="card h-100">
-                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp" class="card-img-top" alt="Skyscrapers" />
-                    <div class="card-body">
-                        <h5 class="card-title">John Doe</h5>
-                        <p class="card-text">
-                            Phone: <a href='tel:+12345'>12345</a> <br />
-                            Email: <a href='mailto:john@test.co'>john@test.co</a>
-                        </p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted">Last updated 3 mins ago</small>
-                    </div>
-                </div>
-            </div>
+            {sample_data.map((sample) => {
+                const col = (
+                    <>
+                        <div class="col">
+                            <div class="card h-100">
+                                <img src={sample.img} class="card-img-top" alt="contact-logo" style={{width: "", objectFit: "contain"}} />
+                                <div class="card-body">
+                                    <h5 class="card-title">{sample.name}</h5>
+                                    <p class="card-text">
+                                        Phone: <a href='tel:+12345'>{sample.phone}</a> <br />
+                                        Email: <a href='mailto:john@test.co'>{sample.email}</a>
+                                    </p>
+                                </div>
+                                <div class="card-footer">
+                                    <small class="text-muted">{sample.last_updated}</small>
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                )
+                return col
+            })
+            }
 
 
-            <div class="col">
-                <div class="card h-100">
-                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp" class="card-img-top" alt="Skyscrapers" />
-                    <div class="card-body">
-                        <h5 class="card-title">John Doe</h5>
-                        <p class="card-text">
-                            Phone: <a href='tel:+12345'>12345</a> <br />
-                            Email: <a href='mailto:john@test.co'>john@test.co</a>
-                        </p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted">Last updated 3 mins ago</small>
-                    </div>
-                </div>
-            </div>
 
-
-            <div class="col">
-                <div class="card h-100">
-                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp" class="card-img-top" alt="Skyscrapers" />
-                    <div class="card-body">
-                        <h5 class="card-title">John Doe</h5>
-                        <p class="card-text">
-                            Phone: <a href='tel:+12345'>12345</a> <br />
-                            Email: <a href='mailto:john@test.co'>john@test.co</a>
-                        </p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted">Last updated 3 mins ago</small>
-                    </div>
-                </div>
-            </div>
 
 
 
---
+++ src/data_sample.js
@@ -0,0 +1,27 @@
+const sample_data = [{
+    name: "John",
+    phone: 12345,
+    email: "john@test.com",
+    last_updated: "21Sep2021 08:40",
+    img: "https://source.unsplash.com/random/800x600?man"
+}, {
+    name: "Micahel",
+    phone: 654321,
+    email: "michael@test.com",
+    last_updated: "21Jan2022 12:40",
+    img: "https://source.unsplash.com/random/800x600?women"
+}, {
+    name: "Mike",
+    phone: 90912345,
+    email: "mike@test.com",
+    last_updated: "21Sep2022 08:40",
+    img: "https://source.unsplash.com/random/800x600?girl"
+}, {
+    name: "John",
+    phone: 12345,
+    email: "john@test.com",
+    last_updated: "21Sep2022 08:40",
+    img: "https://source.unsplash.com/random/800x600?boy"
+}]
+
+export default sample_data
