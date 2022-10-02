4fc78875ee063bc0a7bba6d28c0cd2b64a018e2e
mfaiz6 <Mohammafaiz_2019bcse002@nitsri.net>
2022-10-02 15:53:51 +0530

    minor fixes

--- src/data_sample.js
+++ src/data_sample.js
@@ -3,25 +3,31 @@ const sample_data = [{
     phone: 12345,
     email: "john@test.com",
     last_updated: "21Sep2021 08:40",
-    img: "https://source.unsplash.com/random/800x600?man"
+    img: "https://source.unsplash.com/random/800x600"
 }, {
     name: "Micahel",
     phone: 654321,
     email: "michael@test.com",
     last_updated: "21Jan2022 12:40",
-    img: "https://source.unsplash.com/random/800x600?women"
+    img: "https://source.unsplash.com/random/800x600?dark"
 }, {
     name: "Mike",
     phone: 90912345,
     email: "mike@test.com",
     last_updated: "21Sep2022 08:40",
-    img: "https://source.unsplash.com/random/800x600?girl"
+    img: "https://source.unsplash.com/random/800x600?green"
 }, {
     name: "John",
     phone: 12345,
     email: "john@test.com",
     last_updated: "21Sep2022 08:40",
-    img: "https://source.unsplash.com/random/800x600?boy"
+    img: "https://source.unsplash.com/random/800x600?water"
+}, {
+    name: "John",
+    phone: 12345,
+    email: "john@test.com",
+    last_updated: "21Sep2022 08:40",
+    img: "https://source.unsplash.com/random/800x600?nature"
 }]
 
 export default sample_data
